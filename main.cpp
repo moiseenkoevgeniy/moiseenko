3580a0a1c861e3a5f309f9774dfafe21b47c6082
moiseenkoevgeniy <144607923+moiseenkoevgeniy@users.noreply.github.com>
2024-04-16 19:17:50 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,159 +1,176 @@
-#include <cmath>
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <Windows.h>
 
 using namespace std;
 
-struct Point {
-    int x;
-    int y;
-};
+// Функція для видалення елементів з масиву, які зустрічаються більше двох разів
+void removeDuplicates(vector<int>& arr) {
+    unordered_map<int, int> frequency;
+    vector<int> result;
 
-struct Figure {
-    int color;
-    Point center;
-    int radius;
-};
+    // Рахуємо кількість зустрічей кожного елемента
+    for (int num : arr) {
+        frequency[num]++;
+    }
 
-bool isInsideCircle(Point point, Figure figure) {
-    return pow(point.x - figure.center.x, 2) + pow(point.y - figure.center.y, 2) <= pow(figure.radius, 2);
+    // Заповнюємо результат лише елементами, що зустрічаються не більше двох разів
+    for (int num : arr) {
+        if (frequency[num] <= 2) {
+            result.push_back(num);
+        }
+    }
+
+    // Переприсвоюємо масив arr результатом
+    arr = result;
 }
 
-double factorial(int num) {
-    if (num == 0 || num == 1) {
-        return 1;
-    } else {
-        double fact = 1;
-        for (int i = 2; i <= num; ++i) {
-            fact *= i;
+// Функція для сортування масиву методом двійкового вставлення
+void binaryInsertionSort(vector<int>& arr) {
+    int n = arr.size();
+    for (int i = 1; i < n; ++i) {
+        int key = arr[i];
+        int left = 0, right = i - 1;
+
+        // Використовуємо бінарний пошук для знаходження позиції для вставки
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] > key)
+                right = mid - 1;
+            else
+                left = mid + 1;
         }
-        return fact;
-    }
-}
 
-void taskOne() {
-    cout << "Вы выбрали задание 1" << endl;
+        // Переміщуємо всі елементи більші за key на одну позицію вправо
+        for (int j = i - 1; j >= left; --j)
+            arr[j + 1] = arr[j];
 
-    Point point;
-    cout << "Введите координаты точки: ";
-    cin >> point.x >> point.y;
-
-    int color;
-    cout << "Введите цвет фигуры: ";
-    cin >> color;
-
-    Figure figures[] = {
-        {12, {5, 5}, 5},
-    };
-
-    int i;
-    for (i = 0; i < 1; i++) {
-        if (figures[i].color == color) {
-            break;
-        }
-    }
-
-    int pointsInside = 0;
-
-    for (int j = 0; j < 5; j++) {
-        Point tempPoint;
-        cout << "Введите координаты точки " << j + 1 << ": ";
-        cin >> tempPoint.x >> tempPoint.y;
-
-        bool isInside = isInsideCircle(tempPoint, figures[i]);
-        if (isInside) {
-            pointsInside++;
-        }
-    }
-
-    cout << "Количество точек, попавших в фигуру: " << pointsInside << endl;
-}
-
-void taskTwo() {
-    cout << "Вы выбрали задание 2" << endl;
-
-    double x;
-    int n;
-
-    cout << "Введите действительное число x: ";
-    cin >> x;
-
-    cout << "Введите натуральное число n: ";
-    cin >> n;
-
-    double sum = 0;
-    for (int i = 1; i <= n; i++) {
-        sum += pow(-1, i) * pow(-1 + cos(x), i) / i;
-    }
-
-    cout << "Значение выражения: " << sum << endl;
-
-    if (n % 2 == 0) {
-        for (int i = 1; i <= n; i++) {
-            if (i % 3 == 0) {
-                cout << "Значение " << i << "-го элемента: " << sum / i << endl;
-            }
-        }
-    } else {
-        for (int i = 1; i <= n; i++) {
-            if (i % 4 == 0) {
-                cout << "Значение " << i << "-го элемента: " << sum / i << endl;
-            }
-        }
+        // Вставляємо елемент у відповідну позицію
+        arr[left] = key;
     }
 }
 
-void taskThree() {
-    cout << "Вы выбрали задание 3" << endl;
-
-    int n;
-    cout << "Введите n: ";
-    cin >> n;
-
-    double s = 0;
-    for (int i = 1; i <= n; ++i) {
-        s += (3 * i - 2) / factorial(100 * (i + 1) + 2);
+// Функція для зчитування масиву з файлу
+vector<int> readArrayFromFile(const string& filename) {
+    vector<int> arr;
+    ifstream inputFile(filename);
+    if (!inputFile.is_open()) {
+        cerr << "Unable to open file: " << filename << endl;
+        return arr; // Повертаємо пустий масив у разі помилки
     }
 
-    double e = 1e-5;
-    double g = 1e5;
-    if (abs(s) < e) {
-        cout << "Ряд совпадает." << endl;
-    } else if (abs(s) > g) {
-        cout << "Ряд разбегается." << endl;
-    } else {
-        cout << "Невозможно определить сходимость ряда." << endl;
+    int number;
+    while (inputFile >> number)
+        arr.push_back(number);
+
+    inputFile.close();
+    return arr;
+}
+
+// Функція для виведення масиву у консоль
+void printArray(const vector<int>& arr) {
+    for (int num : arr)
+        cout << num << " ";
+    cout << endl;
+}
+
+// Функція для зчитування матриці з файлу
+vector<vector<int>> readMatrixFromFile(const string& filename, int& M, int& N) {
+    ifstream file(filename);
+    if (!file.is_open()) {
+        cerr << "Помилка відкриття файлу.\n";
+        exit(1);
     }
+
+    file >> M >> N;
+    vector<vector<int>> matrix(M, vector<int>(N));
+
+    for (int i = 0; i < M; ++i) {
+        for (int j = 0; j < N; ++j) {
+            file >> matrix[i][j];
+        }
+    }
+
+    file.close();
+    return matrix;
+}
+
+// Функція для порівняння двох рядків матриці
+bool areSimilar(const vector<int>& row1, const vector<int>& row2) {
+    if (row1.size() != row2.size()) return false;
+
+    unordered_set<int> set1(row1.begin(), row1.end());
+    unordered_set<int> set2(row2.begin(), row2.end());
+
+    return set1 == set2;
 }
 
 int main() {
     int choice;
 
     do {
-        cout << "Выберите задачу:" << endl;
-        cout << "1. Задание 1" << endl;
-        cout << "2. Задание 2" << endl;
-        cout << "3. Задание 3" << endl;
-        cout << "0. Выйти" << endl;
-        cout << "Ваш выбор: ";
+        cout << "Меню:" << endl;
+        cout << "1. Виконати програму для видалення зайвих елементів з масиву" << endl;
+        cout << "2. Виконати програму для порівняння рядків матриці" << endl;
+        cout << "3. Виконати програму для сортування масиву методом двійкового вставлення" << endl;
+        cout << "4. Вийти з програми" << endl;
+        cout << "Виберіть опцію: ";
         cin >> choice;
 
-        switch (choice) {
-        case 1:
-            taskOne();
-            break;
-        case 2:
-            taskTwo();
-            break;
-        case 3:
-            taskThree();
-            break;
-        case 0:
-            cout << "До свидания!" << endl;
-            break;
-        default:
-            cout << "Неверный выбор. Попробуйте еще раз." << endl;
+        switch(choice) {
+            case 1: {
+                vector<int> myArray = readArrayFromFile("C:\Users\Users\Desktop\in_17.txt");
+                if (myArray.empty()) {
+                    cerr << "Array is empty or file could not be read." << endl;
+                    break;
+                }
+                cout << "Unsorted Array: ";
+                printArray(myArray);
+                removeDuplicates(myArray);
+                cout << "Array after removing duplicates: ";
+                printArray(myArray);
+                break;
+            }
+            case 2: {
+                int M, N;
+                vector<vector<int>> matrix;
+                matrix = readMatrixFromFile("C:\Users\Users\Desktop\math_in_36.txt", M, N);
+                if (M == 0 || N == 0) {
+                    cout << "Матриця порожня.\n";
+                    break;
+                }
+                int similarCount = 0;
+                for (int i = 1; i < M; ++i) {
+                    if (areSimilar(matrix[0], matrix[i])) {
+                        similarCount++;
+                    }
+                }
+                cout << "Кількість рядків, схожих на перший рядок: " << similarCount << "\n";
+                break;
+            }
+            case 3: {
+                vector<int> myArray = readArrayFromFile("C:\Users\Users\Desktop\in_17.txt");
+                if (myArray.empty()) {
+                    cerr << "Array is empty or file could not be read." << endl;
+                    break;
+                }
+                cout << "Unsorted Array: ";
+                printArray(myArray);
+                binaryInsertionSort(myArray);
+                cout << "Sorted Array: ";
+                printArray(myArray);
+                break;
+            }
+            case 4:
+                cout << "До побачення!" << endl;
+                break;
+            default:
+                cout << "Невірний вибір. Будь ласка, виберіть знову." << endl;
         }
-    } while (choice != 0);
+    } while (choice != 4);
 
     return 0;
 }
